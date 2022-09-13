e95215a484dd1bbf7c2331358abc84ee2cdcf431
Honza Pospisil <me@honzapospisil.com>
2022-09-13 13:32:07 +0200

    Added prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import {join} from 'path';
+import { join } from "path";
 
 const infoLogFilename: string = join(__dirname, "log/info.log");
 
 console.log(infoLogFilename);
+
+const u = 1;
+
+if (u) {
+}
