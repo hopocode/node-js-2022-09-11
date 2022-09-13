148accc3367b3d05c78e8a8cb73e1f47675a462e
Honza Pospisil <me@honzapospisil.com>
2022-09-13 13:49:28 +0200

    Setrup basic htp server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { join } from "path";
+import {createServer, IncomingMessage, ServerResponse} from 'http';
 
-const infoLogFilename: string = join(__dirname, "log/info.log");
+// const infoLogFilename: string = join(__dirname, "log/info.log");
 
-console.log(infoLogFilename);
+// console.log(infoLogFilename);
 
-const u = 1;
+// const u = 1;
 
-if (u) {
-}
+// if (u) {
+// }
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    res.end("Ahoj");
+});
+
+server.listen(3500, () => {
+    console.log("Server start");
+});
