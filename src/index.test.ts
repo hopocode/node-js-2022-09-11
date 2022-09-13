b25ffa5745a08c34059e19a76d57f01b37911a21
Honza Pospisil <me@honzapospisil.com>
2022-09-13 13:15:40 +0200

    Setum libs

---
+++ src/index.test.ts
@@ -0,0 +1,3 @@
+test("xxx", () => {
+    expect(true).toBe(true);
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import {join} from 'path';
+
+const infoLogFilename: string = join(__dirname, "log/info.log");
+
+console.log(infoLogFilename);
